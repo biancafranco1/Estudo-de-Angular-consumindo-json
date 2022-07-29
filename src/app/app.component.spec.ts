f678cea20a6d2230ca4049d1911e5f27ed756130
Bianca <bianca-franco1@hotmail.com>
2022-07-28 22:17:51 -0300

    projeto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'projeto'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projeto');
+  //  expect(app.title).toEqual('projeto');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'projeto';
+  /*- Propriedades do componente -*/
+  lista: Usuario[] = []
+  termo:string=''
+  /*------------------------------*/
+
+  /**
+   * Função executada na inicialização do componente
+   */
+  ngOnInit() {
+      const getdata = async () => {
+      let response = await fetch('../assets/json/dados_teste.json')
+      let registros = await response.json()
+      this.lista = registros.data;
+    }
+    getdata()
+  }
+
+  search(){
+console.log(this.termo);
+
+  }
+
+}
+//interface para definir as propriedades de um usuário
+interface Usuario {
+  id: number;
+  abbreviation: string;
+  city: string;
+  conference: string;
+  division: string;
+  full_name: string;
+  name: string;
 }
